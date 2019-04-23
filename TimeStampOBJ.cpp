2a1464f42e29b33e61adc89ce9ba0df46affaa6b
YC Concept <49748068+YannChansigaud@users.noreply.github.com>
2019-04-23 20:35:39 +0200

    Add files via upload

---
+++ TimeStampOBJ.cpp
@@ -0,0 +1,60 @@
+/*
+ * TimeStampOBJ.cpp
+ * CREATED on WEDNESDAY 17 APRIL 2019
+ * by CHANSIGAUD YANN
+ */
+
+
+#if (ARDUINO <  100)
+#include <WProgram.h>
+#else
+#include <Arduino.h>
+#endif
+#include <inttypes.h>
+
+#include "TimeOBJ.h"
+#include "TimeStampOBJ.h"
+
+TimeStampOBJ::TimeStampOBJ(){}
+
+TimeOBJ &TimeStampOBJ::getTimeFromLastCheck(){    compute();  return( past );    }       // compute and return past value
+TimeOBJ &TimeStampOBJ::getTimeFromBeginning(){    compute();  return( now  );    }       // compute and return now value
+
+uint8_t TimeStampOBJ::getNbLoop(){    return( nbLoop );    }                             // 
+void TimeStampOBJ::compute(){                                                            // MAIN CALCULATION METHOD
+    TNOW = micros();                                                                     // calling back values from micros()
+	if( TNOW < TPREV ){                                                                  // If last call was greater than this call 
+		has_loop = true;                                                                 //   it means that micros has rolling back to zero
+		nbLoop++;                                                                        //   nbLoop increased
+	}                                                                                    // 
+	else{                                                                                // Else
+		has_loop = false;                                                                //   
+	}                                                                                    // 
+	if( has_loop ){    TPASS = TNOW - 0 + ( MAX_UINT32_T - TPREV );    }                 // If rolling back, count from last one to the end plus from the begining to now
+	else{              TPASS = TNOW - TPREV;                           }                 // If not, just count from last to now
+	now.add( TPASS, MICRO );                                                             // Adding elapsed time to TimeOBJ now
+	past = 0;                                                                            // Setting TimeOBJ past to zero
+	past.add( TPASS, MICRO );                                                            // Store elapsed time value to TimeOBJ past
+	TPREV = TNOW;                                                                        // record this time point to TPREV
+}                                                                                        // 
+
+bool TimeStampOBJ::operator>=( uint32_t val ){                return( !(*this<val) );  }                  // To compare >= return the opposite <
+bool TimeStampOBJ::operator<=( uint32_t val ){                return( !(*this>val) );  }                  // 
+bool TimeStampOBJ::operator>=( const TimeOBJ &TimeToCompare ){    return( !(*this<TimeToCompare) );  }    // 
+bool TimeStampOBJ::operator<=( const TimeOBJ &TimeToCompare ){    return( !(*this>TimeToCompare) );  }    // 
+
+bool TimeStampOBJ::operator>(  uint32_t val ){          // compare a uint32_t value ( like millis() ) to TimeStampOBJ
+	compute();                                          // compute values
+	past = 0;											// reset temporary TimeOBJ object
+	past.add( val, TimeStampOBJ_defaultFormat );		// adding parsed value 
+	return( now>past );									// return result
+}
+
+bool TimeStampOBJ::operator<(  uint32_t val ){          // compare a uint32_t value ( like millis() ) to TimeStampOBJ
+	compute();                                          // compute values
+	past = 0;											// reset temporary TimeOBJ object
+	past.add( val, TimeStampOBJ_defaultFormat );		// adding parsed value
+	return( now<past );									// return result
+}
+bool TimeStampOBJ::operator>(  const TimeOBJ &TimeToCompare ){		return( now>TimeToCompare );	}
+bool TimeStampOBJ::operator<(  const TimeOBJ &TimeToCompare ){		return( now<TimeToCompare );	}
---
+++ TimeStampOBJ.h
@@ -0,0 +1,70 @@
+/*
+ TimeStampOBJ.h
+ CREATED on WEDNESDAY 17 APRIL 2019
+ by CHANSIGAUD YANN
+
+TimeStampOBJ available on https://github.com/YannChansigaud/TimeStampOBJ
+TimeStampOBJ use TimeOBJ available on : https://github.com/YannChansigaud/TimeOBJ
+
+TimeStampOBJ is designed to define a time mark. All internal calculation are using microsecond with micros()
+
+WARNING : all values sended to TimeStampOBJ with overloaded comparison operator ( >, <, >=, <= ) are evaluated as millisecond in view to correspond to millis()
+          define TimeStampOBJ_defaultFormat to "MICRO" if you want to pass microseconds values
+
+PAY ATTENTION : this class has to be called with compute() at least 1 time per hours
+                maximum delay between two calls : 1h 11min 34s 967ms 295us (H/M/S/ms/us equivalent for the maximum of uint32_t length return by micros() )
+
+- This class didn't reset micros().
+- micros() rollback to zero by itself when uint32_t maximum reached
+
+This class use 2 TimeOBJ objects
+- TimeOBJ now : Time object to store total execution time since the begining 
+- TimeOBJ pass : Time object to store elapsed time since last call
+                 This object is use to store temporary values used by comparison operator
+
+- TNOW, TPREV, TPASS : uint32_t variables used for calculation with micros()
+
+*/
+#ifndef TimeStampOBJ_h
+#define TimeStampOBJ_h
+#include <inttypes.h>
+#include "TimeOBJ.h"
+
+#define MAX_UINT32_T   4294967295
+
+//#define TimeStampOBJ_defaultFormat   MICRO
+#define TimeStampOBJ_defaultFormat   MILLI
+
+class TimeStampOBJ{
+  public :
+    TimeStampOBJ();
+	// ==================================================== //       Values get back methods
+	TimeOBJ &getTimeFromLastCheck();                        // since last call
+	TimeOBJ &getTimeFromBeginning();                        // since the begenning
+	// ==================================================== //       Overloaded comparison operator with values
+	bool operator>=( uint32_t val );                        // >=
+    bool operator<=( uint32_t val );                        // <=
+    bool operator>(  uint32_t val );                        // >
+    bool operator<(  uint32_t val );                        // <
+    // ==================================================== //       Overloaded comparison operator with TimeOBJ objects
+    bool operator>=( const TimeOBJ &TimeToCompare );        // >=
+    bool operator<=( const TimeOBJ &TimeToCompare );        // <=
+    bool operator>(  const TimeOBJ &TimeToCompare );        // >
+    bool operator<(  const TimeOBJ &TimeToCompare );        // <
+    // ==================================================== //       
+    uint8_t getNbLoop();                                    // Accessor to nbLoop of micros() counted
+    void compute();                                         // Method called at least 1 time per hour
+
+	
+
+  private :
+    TimeOBJ now     = TimeOBJ();
+    TimeOBJ past    = TimeOBJ();
+    uint32_t TPREV  = 0;
+	uint32_t TNOW   = 0;
+	uint32_t TPASS  = 0;
+	uint8_t  nbLoop = 0;
+	bool has_loop = false;
+};
+
+#endif
